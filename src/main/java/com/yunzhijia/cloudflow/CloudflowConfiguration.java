4d987c7991ce025f1079625c108bd39c8f121f92
bszxhxf <44159811+bszxhxf@users.noreply.github.com>
2020-04-30 17:42:19 +0800

    提交代码
    提交代码

---
+++ src/main/java/com/yunzhijia/cloudflow/CloudflowConfiguration.java
@@ -0,0 +1,9 @@
+package com.yunzhijia.cloudflow;
+
+public class CloudflowConfiguration {
+    public String appId;
+    public String secret;
+    public String eid;
+    public String key;
+    public String fileSecret;
+}
